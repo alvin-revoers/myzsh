365e3a4409f0243016c827849bbaaa4617963eab
alvin-revoers <86232444+alvin-revoers@users.noreply.github.com>
2024-08-20 06:38:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # check if .zshrc file exits
-File=~/home/$USER/.zshrc
+File=~/$home/$USER/.zshrc
 if [ -f "$File" ]; then
 	echo "$File exists."
-	mv ~/home/$USER/.zshrc /home/$USER/zshrc-backup.sh
+	mv ~/$home/$USER/.zshrc /home/$USER/zshrc-backup.sh
 	echo "old .zshrc moved to zshrc-backup.sh"
 fi
 
 url='https://raw.githubusercontent.com/jakbin/myzsh/main/.zshrc'
-wget -P /home/$USER/ $url
+wget -P ~/$home/$USER/ $url
 echo ".zshrc file downloaded"
